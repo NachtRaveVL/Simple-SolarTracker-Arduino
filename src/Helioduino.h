006b51223a9331db964419d36c110edfb571ac53
NachtRaveVL <nachtravevl@gmail.com>
2023-03-04 15:19:36 -0800

    bumping minor vers for now

--- src/Helioduino.h
+++ src/Helioduino.h
@@ -22,7 +22,7 @@
     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
     OTHER DEALINGS IN THE SOFTWARE.
 
-    Simple-SolarTracker-Arduino - Version 0.6
+    Simple-SolarTracker-Arduino - Version 0.6.0.1
 */
 
 #ifndef Helioduino_H
