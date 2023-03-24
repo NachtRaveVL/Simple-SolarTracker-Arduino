a6213a82c04bf349bb20fc439bd71afa03b93736
NachtRaveVL <nachtravevl@gmail.com>
2023-03-23 23:27:18 -0700

    panels menu add

---
+++ src/shared/screens/HelioMenuPanels.h
@@ -0,0 +1,19 @@
+/*  Helioduino: Simple automation controller for solar tracking systems.
+    Copyright (C) 2023 NachtRaveVL          <nachtravevl@gmail.com>
+    Helioduino Panels Menu Screen
+*/
+
+#include <Helioduino.h>
+#ifdef HELIO_USE_GUI
+#ifndef HelioMenuPanels_H
+#define HelioMenuPanels_H
+
+class HelioPanelsMenu;
+
+#include "../HelioduinoUI.h"
+
+class HelioPanelsMenu : public HelioMenu
+{ }; // todo
+
+#endif // /ifndef HelioMenuPanels_H
+#endif
